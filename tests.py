b28aa57ae263e0642845de1bbc5da5f17f17892f
Colin Joss <colindjoss@gmail.com>
2022-05-11 14:27:41 -0700

    Linting errors corrected in tests

--- tests.py
+++ tests.py
@@ -1,12 +1,12 @@
 import unittest
-import task.py
+import task
 
 
 class TestCase(unittest.TestCase):
-  def test1(self):
-    expected = 'Hello World'
-    self.assertEqual(task.my_func(), expected)
-    
+    def test1(self):
+        expected = 'Hello World'
+        self.assertEqual(task.my_func(), expected)
+
+
 if __name__ == '__main__':
-  unittest.main()
-  
+    unittest.main()
